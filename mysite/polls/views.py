5c4bbe5a6ac3207af2d3c3a369089d54b0145852
KOngTNP <tanapol.bu@ku.th>
2020-09-19 21:59:52 +0700

    add valid vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,11 @@
 from django.utils import timezone
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render , redirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+from django.contrib import messages
+
 
 from .models import Choice, Question
 
@@ -13,7 +16,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 
@@ -49,11 +52,10 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+
+def valid_vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if not question.can_vote():
+        messages.error(request, f'You are not allowed to vote in the "{question.question_text}" poll!')
+        return redirect('polls:index')
+    return render(request, 'polls/detail.html', {'question': question})
