333cdc287ffd9c4ec5e69f1328346e9f929d7172
KOngTNP <tanapol.bu@ku.th>
2020-09-19 21:57:03 +0700

    add end_data and pub_data

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,17 +1,30 @@
 import datetime
+from datetime import date, timedelta
 
+from django import forms
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published' ,default=timezone.now)
+    end_date = models.DateTimeField('ending date', default=timezone.now() + datetime.timedelta(days=10))
+    
+
     def __str__(self):
         return self.question_text
+        
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+
+    def is_published(self):
+        return timezone.now() >= self.pub_date
+
+    def can_vote(self):
+        return self.end_date > timezone.now() >= self.pub_date
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
