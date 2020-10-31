1267b9a09d880d197b2c90051fca63d3348ddbcf
KOngTNP <tanapol.bu@ku.th>
2020-10-31 12:19:38 +0700

    add urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('', include('polls.urls', namespace='loginpage')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),  
+
 ]
